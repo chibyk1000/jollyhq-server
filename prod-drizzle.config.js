c5581b742fdf8f808266fc2eb1b3907da22b1e95
Okorie Chibuike <88936315+chibyk1000@users.noreply.github.com>
2026-07-09 03:35:38 +0100

    updated

---
+++ prod-drizzle.config.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./dist/db/schema/**/*js",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+}); 
